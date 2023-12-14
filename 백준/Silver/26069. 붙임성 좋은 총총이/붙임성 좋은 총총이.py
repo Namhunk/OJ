f524bd0c955f017eddee876fad29774f403d10e1
Namhunk <151957749+Namhunk@users.noreply.github.com>
2023-12-14 17:58:57 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 124 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline().strip())
+
+dance = set()
+dance.add("ChongChong")
+for _ in range(n):
+    a, b = sys.stdin.readline().strip().split()
+
+    if a in dance or b in dance:
+        dance.add(a)
+        dance.add(b)
+
+print(len(dance))
