a6197273778fb3b2f4fc4f4ab0e4d6d09f88137e
Namhunk <151957749+Namhunk@users.noreply.github.com>
2023-12-14 17:57:51 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 6, Time: 812 ms, Memory: 86900 KB -BaekjoonHub

---
+++ 백준/Silver/24484. 알고리즘 수업 － 깊이 우선 탐색 6/알고리즘 수업 － 깊이 우선 탐색 6.py
@@ -0,0 +1,37 @@
+import sys
+
+sys.setrecursionlimit(10**6)
+def dfs(r, cnt):
+    depth[r] = cnt
+    for i in sorted(graph[r], reverse= True):
+        if depth[i] < 0:
+            dfs(i, cnt + 1)
+
+n, m, r = map(int, sys.stdin.readline().strip().split())
+graph = {i: [] for i in range(n+1)}
+
+for _ in range(m):
+    i, j = map(int, sys.stdin.readline().strip().split())
+    
+    graph[i].append(j)
+    graph[j].append(i)
+
+visited = [0] * (n+1)
+depth = [-1] * (n+1)
+cnt = 1
+dfs(r, 0)
+
+stack = [r]
+while stack:
+    v = stack.pop()
+    if not visited[v]:
+        visited[v] = cnt
+        cnt += 1
+        for i in sorted(graph[v]):
+            stack.append(i)
+
+ans = 0
+for i in range(1, n+1):
+    ans += (visited[i] * depth[i])
+
+print(ans)
