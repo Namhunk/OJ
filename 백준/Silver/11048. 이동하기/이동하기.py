014e88457d77016059c3e5acad0cd8714541c4c0
Namhunk <151957749+Namhunk@users.noreply.github.com>
2023-12-14 17:34:22 +0900

    [Silver II] Title: 이동하기, Time: 900 ms, Memory: 79464 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -0,0 +1,11 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().strip().split())
+dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
+arr = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(n)]
+
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        dp[i][j] = max(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + arr[i-1][j-1]
+    
+print(dp[n][m])
