92a01cc5907a783c06f56d2cfa28c77131a4b853
Namhunk <151957749+Namhunk@users.noreply.github.com>
2023-12-19 18:01:30 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 400 ms, Memory: 128448 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -2,31 +2,27 @@ import sys, heapq
 
 # 최소 스패닝 트리의 가중치 출력
 
-# kruskal MST         
+# prim MST
 
-def find(x):
-    if x != parent[x]: parent[x] = find(parent[x])
-    return parent[x]
-
-def union(x, y):
-    x, y = find(x), find(y)
-    if x < y: parent[y] = x
-    else: parent[x] = y
-
-# 정점의 개수 V, 간선의 수 E
+# V, E
 V, E = map(int, sys.stdin.readline().strip().split())
-# 간선 정보 입력
-parent = [i for i in range(V+1)]
-ans = 0
-heap = []
+graph = [[] for _ in range(V+1)]
 for _ in range(E):
     A, B, C = map(int, sys.stdin.readline().strip().split())
-    heapq.heappush(heap, (C, A, B))
+    graph[A].append((C, B))
+    graph[B].append((C, A))
+
+ans = 0
+heap = [(0, 1)]
+visit = [0] + [1]*(V)
 
 while heap:
-    w, a, b = heapq.heappop(heap)
-    if find(a) != find(b):
-        union(a, b)
-        ans += w
-    
+    cw, cn = heapq.heappop(heap)
+    if visit[cn]:
+        visit[cn] = 0
+        ans += cw
+        for nw, nn in graph[cn]:
+            if visit[nn]:
+                heapq.heappush(heap, (nw, nn))
+
 print(ans)
