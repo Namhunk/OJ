3e7529af7a1c31423a58b90ca5b3f569e180a6b8
Namhunk <151957749+Namhunk@users.noreply.github.com>
2024-01-02 19:48:25 +0900

    [Gold IV] Title: 연구소, Time: 1060 ms, Memory: 114920 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,62 +1,53 @@
-import sys, copy
 from collections import deque
+from itertools import combinations
+import sys
 
-# 연구소의 크기는 N x M 인 직사각형 
-# 바이러스는 상하좌우 인접한 빈 칸으로 퍼져나감
-# 새로 세울 수 있는 벽의 개수는 3개 (*반드시 3개를 세워야함*)
-# 빈칸 = 0, 벽 = 1, 바이러스 = 2
+# 다른 방식 itertools의 cominations 사용
+# N, M 입력
+N, M = map(int, sys.stdin.readline().strip().split())
+# 실험실 입력
+lab = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(N)]
 
-# 연구소의 지도가 주어졌을 때 얻을 수 있는 안전 영역의 크기의 최댓값
+empty = set() # 빈 공간
+virus = set() # 바이러스
 
-# 벽을 설치한다 or 벽을 설치하지 않는다.
-# 벽 3개를 설치 했다면 테스트를 해본다.
-def Create_Wall(cnt):
-    if cnt < 3:
-        for i in range(N):
-            for j in range(M):
-                if not lab[i][j]:
-                    lab[i][j] = 1
-                    Create_Wall(cnt+1)
-                    lab[i][j] = 0
-    else:
-        BFS()
-    
-def BFS():
-    global ans
-    safe_area = 0
-    temp_lab = copy.deepcopy(lab)
-    visit = set(virus)
+# 실험실에서 각각 요소들의 위치를 저장
+for i in range(N):
+    for j in range(M):
+        if not lab[i][j]: empty.add((i, j))
+        elif lab[i][j] == 2: virus.add((i, j))
+
+ans = 0 # 정답
+move = [(-1, 0), (1, 0), (0, -1), (0, 1)] # 이동 방향
+
+# combinations를 사용해 빈 공간 3개를 뽑아냄
+for pos in combinations(empty, 3):
+    add = set(pos) # 추가로 막을 부분
+    visit = set(virus) # 방문 표시
     que = deque(virus)
     
     while que:
         x, y = que.popleft()
         for r, c in move:
-            if 0 <= x+r < N and 0 <= y+c < M and (x+r, y+c) not in visit:
-                if not temp_lab[x+r][y+c]:
-                    temp_lab[x+r][y+c] = 2
-                    visit.add((x+r, y+c))
-                    que.append((x+r, y+c))
+            # 다음 위치가 실험실 범위 안에 있고, 빈 공간인가?
+            if not (0 <= x+r < N and 0 <= y+c < M and not lab[x+r][y+c]): continue
+            # 생성한 벽의 위치와 다른가?
+            if (x+r, y+c) in add: continue
+            # 방문하지 않은 위치인가?
+            if (x+r, y+c) in visit: continue
+            
+            visit.add((x+r, y+c))
+            que.append((x+r, y+c))
     
+    area = 0 # 현재 안전 구역의 크기
     for i in range(N):
         for j in range(M):
-            if not temp_lab[i][j]: safe_area += 1
-    
-    ans = max(ans, safe_area)
-        
-# 세로 N, 가로 M
-N, M = map(int, sys.stdin.readline().strip().split())
-lab = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(N)]
+            if lab[i][j]: continue
+            if (i, j) in add: continue
+            if (i, j) in visit: continue
+            area += 1
+            
+    ans = max(ans, area)
 
-# 바이러스의 이동 방향
-move = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-# 바이러스의 위치를 구해놓음
-virus = []
-for i in range(N):
-    for j in range(M):
-        if lab[i][j] == 2: virus.append((i, j))
-
-# 정답
-ans = 0
-Create_Wall(0)
 print(ans)
+            
