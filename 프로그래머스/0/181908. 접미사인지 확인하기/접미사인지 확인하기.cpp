13ef1d5e993811d399f394406b6693e05743d7cb
Namhunk <151957749+Namhunk@users.noreply.github.com>
2024-07-04 19:16:03 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(string my_string, string is_suffix) {
+    int answer = 0;
+    for (int i=0; i<my_string.length(); i++)
+        if (my_string.substr(i) == is_suffix) answer = 1;
+    return answer;
+}
