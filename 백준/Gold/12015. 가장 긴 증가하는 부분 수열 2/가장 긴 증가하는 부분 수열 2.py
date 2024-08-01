1e22108c3903461f3036db37fa4ded3cad527a58
Namhunk <151957749+Namhunk@users.noreply.github.com>
2024-08-01 15:10:33 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 572 ms, Memory: 251096 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
@@ -1,7 +1,6 @@
 import sys
 input = sys.stdin.readline
 
-from bisect import bisect_left
 # 첫째 줄에 수열 A의 가장 긴 증가하는 부분 수열의 길이를 출력
 N = int(input().strip()) # 수열 A의 크기 N (1 <= N <= 1,000,000)
 A = list(map(int, input().strip().split())) # 수열 A (1 <= A[i] <= 1,000,000)
@@ -12,7 +11,12 @@ for i in range(1, N):
         seq.append(A[i]) # A[i] 추가
     else: # 작거나 같다면
         l, r = 0, len(seq)-1 # 정답 배열의 범위 안에서
-        idx = bisect_left(seq, A[i])
-        seq[idx] = A[i]
+        while l < r:
+            m = (l+r)//2
+
+            if seq[m] < A[i]: l = m+1
+            else: r = m
+
+        seq[l] = A[i] # 현재 A[i]보다 작은 값으로 교체
 
 print(len(seq))
