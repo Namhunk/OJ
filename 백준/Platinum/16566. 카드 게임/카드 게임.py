e46872043b9d6feaec739b1dd6a870cf847a620d
Namhunk <151957749+Namhunk@users.noreply.github.com>
2024-08-16 16:38:06 +0900

    [Platinum V] Title: 카드 게임, Time: 944 ms, Memory: 453784 KB -BaekjoonHub

--- 백준/Platinum/16566. 카드 게임/카드 게임.py
+++ 백준/Platinum/16566. 카드 게임/카드 게임.py
@@ -1,6 +1,5 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
 
 # K줄에 걸쳐서 수를 출력한다 i번쨰 줄에는 민수가 i번째로 낼 카드의 번호가 출력되어야 한다
 
@@ -8,45 +7,38 @@ sys.setrecursionlimit(10**6)
 N, M, K = map(int, input().strip().split())
 
 # 카드의 번호를 나타내는 M개의 자연수 각 수들은 1 이상 N 이하이며 서로 다르다
-card = sorted(list(map(int, input().strip().split())))
+card = [0]*(N+1) # N 개의 카드
+for i in map(int, input().strip().split()): # 뽑은 카드 입력
+    card[i] = i
 
+before = 0
+tmp = 0
+
+for i in range(N, -1, -1):
+    if card[i]: # 해당 번호의 카드가 있다면
+        tmp = i-1
+
+    card[i] = before # 이전 위치
+    before = tmp
+
+updates = []
 # K개의 자연수, i번째 수는 철수가 i번쨰로 내는 카드의 번호이다. 철수가 내는 카드 역시 1 이상 N이하 이다
-nums = list(map(int, input().strip().split()))
+for i in map(int, input().strip().split()):
+    while i != card[i]:
+        updates.append(i)
+        i = card[i]
+    updates.append(i)
+    i += 1
+    print(i)
+    for j in updates:
+        card[j] = i
+    updates.clear()
+
+
 
 # N개의 빨강, 파랑 카드가 있고 각각 M개의 카드를 선택
 # 철수는 빨간색, 민수는 파란색 카드를 갖는다
 # 이 두명은 M개의 카드 중 1개를 선택, 그리고 두 카드의 숫자 비교를 K번 반복, 카드는 한 번만 사용 가능
 # K번 동안 철수가 낼 카드를 알고 있을 때 철수가 낼 카드보다 큰 카드들 중 가장 작은 카드를 낸다
 
-# 이분 탐색 사용
-def upper_bound(x):
-    l, r = 0, len(card)
-    while l < r:
-        m = (l+r)//2
-
-        if card[find(m)] <= x: l = m+1 # 현재 카드가 K[i]의 카드보다 크도록
-        else: r = m
-
-    return find(l)
-
-# union-find 사용
-parent = [i for i in range(M+1)]
-def find(x):
-    if x != parent[x]: parent[x] = find(parent[x])
-    return parent[x]
-
-def union(x, y):
-    x, y = find(x), find(y)
-    if x > y: x, y = y, x
-    parent[x] = y
-
-for i in range(K):
-    idx = upper_bound(nums[i]) # K[i] 보다 큰 카드의 위치
-
-    if idx >= M: # 범위를 넘어가는 카드라면 앞의 값들 부터 
-        idx = find(0)
-
-    print(card[find(idx)])
-    union(idx, idx+1) # 다음 위치의 카드로 옮김
-
-
+# refer to https://www.acmicpc.net/source/69414123
