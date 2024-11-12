a405cf364d1d73add48f36a5692710faf6063415
Namhunk <151957749+Namhunk@users.noreply.github.com>
2024-11-12 15:24:06 +0900

    [Gold II] Title: 트리의 지름, Time: 364 ms, Memory: 150928 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -1,31 +1,49 @@
 import sys
+input = sys.stdin.readline
 
-def dfs(x, d):
-    global ans
-    
-    s = 0
-    for nx, nd in arr[x]:
-        if visited[nx]:
-            visited[nx] = False
-            result = dfs(nx, nd+d)+nd
-            
-            ans = max(ans, result+s)
-            s = max(s, result)
-            visited[nx] = True
-    
-    return s
+from collections import deque
+# 트리의 지름이란, 트리에서 임의의 두 점 사이의 거리 중 가장 긴 것을 말한다.
+# 트리의 지름을 구하는 프로그램을 작성
 
-V = int(sys.stdin.readline().strip())
-arr = [[] for _ in range(V+1)]
-
+# 트리의 정점의 개수 V (2 <= V <= 100,000)
+V = int(input().strip())
+tree = [[] for _ in range(V+1)]
+# 정점 번호는 1부터 V까지 존재
+# V개의 줄에 걸쳐 간선의 정보
 for _ in range(V):
-    input_ = list(map(int, sys.stdin.readline().strip().split()))
-    
-    for i in range(1, len(input_)-1, 2):
-        arr[input_[0]].append((input_[i], input_[i+1]))
+    row = list(map(int, input().strip().split()))
+    for i in range(1, len(row)-1, 2):
+        tree[row[0]].append((row[i], row[i+1]))
 
-visited = [True]*(V+1)
-visited[1] = False
-ans = 0
-dfs(1, 0)
-print(ans)
+def BFS(start, end = 0): # 시작 위치, 종점
+    que = deque()
+    que.append((start, 0))
+
+    visited = [0]*(V+1)
+
+    res = 0
+    maxNode = 0
+
+    while que:
+        node, dist = que.popleft()
+        visited[node] = 1 # 정점 방문 표시
+
+        if end and node == end: # 종점이 정해졌다면
+            return dist # 거리 반환
+
+        for next_node in tree[node]:
+            if visited[next_node[0]]: # 방문 하지 않은 정점들 만
+                continue
+
+            if res < dist+next_node[1]:
+                res = dist + next_node[1]
+                maxNode = next_node[0]
+
+            que.append((next_node[0], dist+next_node[1]))
+
+    return maxNode
+
+start = BFS(1)
+end = BFS(start)
+
+print(BFS(start, end))
