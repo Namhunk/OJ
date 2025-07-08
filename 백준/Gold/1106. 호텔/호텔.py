c42e6331c0443d9b124ac8d3ceb7c5d3d7b9f0fe
Namhunk <151957749+Namhunk@users.noreply.github.com>
2025-07-08 17:37:41 +0900

    [Gold IV] Title: 호텔, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.py
+++ 백준/Gold/1106. 호텔/호텔.py
@@ -10,24 +10,14 @@ data = [[0, 0]]+[list(map(int, input().strip().split())) for _ in range(N)]
 # 비용, 고객수 적은 순으로 정렬
 data.sort()
 
-# 값을 저장할 DP 배열
-dp = [[float('inf')]*(C+1) for _ in range(N+1)]
+dp = [float('inf') for _ in range(C+100)]
+dp[0] = 0
 
 for i in range(1, N+1):
-    cost = data[i][0] # 각 data[i]의 비용, 고객 수
+    cost = data[i][0]
     cnt = data[i][1]
 
-    for j in range(1, C+1):
-        dp[i][j] = dp[i-1][j] # 이전 행의 최소값
+    for j in range(cnt, C+100):
+        dp[j] = min(dp[j], dp[j-cnt]+cost)
 
-        k = 0 # 해당 비용을 몇 번 사용 하는지
-        while 1:
-            if j-k*cnt <= 0: # 고객의 수가 현재 j값을 넘는다면 비용의 최소 비교
-                dp[i][j] = min(dp[i][j], k*cost)
-                break
-            else:
-                dp[i][j] = min(dp[i][j], dp[i-1][j-k*cnt]+k*cost)
-
-            k+=1
-
-print(dp[N][-1])
+print(min(dp[C:]))
