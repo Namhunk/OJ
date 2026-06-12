b37087bfa5b79772472dccd8a20f1a7c86472ada
Namhunk <151957749+Namhunk@users.noreply.github.com>
2026-06-12 13:11:52 +0900

    [level 3] Title: 2차원 동전 뒤집기, Time: 1.56 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/3/131703. 2차원 동전 뒤집기/2차원 동전 뒤집기.py
+++ 프로그래머스/3/131703. 2차원 동전 뒤집기/2차원 동전 뒤집기.py
@@ -3,46 +3,35 @@ INF = 10**9
 def solution(beginning, target):
     R = len(beginning)
     C = len(beginning[0])
-
-    ans = INF
-
-    # 0 ~ (1<<R)-1: 뒤집을 행 집합을 비트마스크로 표현
-    for mask in range(1 << R):
-        row_flip_cnt = bin(mask).count("1")
-
-        # 열 뒤집기 개수 세기
-        col_flip_cnt = 0
+    
+    answer = INF
+    
+    for mask in range(1 << R): # 뒤집을 행 선택
+        row_cnt = bin(mask).count('1')
+        
+        col_cnt = 0
         ok = True
-
+        
         for c in range(C):
-            # 해당 열의 (행 뒤집기까지 반영된) 현재 열과 타겟 열을 비교
             same = True
             opposite = True
-
             for r in range(R):
-                # 행 r가 뒤집혔으면 1 - beginning[r][c], 아니면 그대로
                 cur = beginning[r][c]
-                if (mask >> r) & 1:
+                if (mask >> r) & 1: # 현재 행이 선택된 경우
                     cur ^= 1
-
+                
                 if cur != target[r][c]:
                     same = False
-                if (cur ^ 1) != target[r][c]:
+                if (cur^1) != target[r][c]:
                     opposite = False
-
-                # 둘 다 이미 불가능이면 더 볼 필요 없음
-                if not same and not opposite:
-                    break
-
+                
             if not same and not opposite:
                 ok = False
                 break
             elif opposite:
-                # 완전히 반대라면, 이 열은 한 번 뒤집어야 함
-                col_flip_cnt += 1
-            # same인 경우는 열을 안 뒤집으면 됨
-
+                col_cnt += 1
+        
         if ok:
-            ans = min(ans, row_flip_cnt + col_flip_cnt)
+            answer = min(answer, row_cnt + col_cnt)
 
-    return ans if ans != INF else -1
+    return answer if answer != INF else -1
